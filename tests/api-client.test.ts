e87ff4ac3d1e6df116c4e9f07cec36a6cd6c1a66
Dexter Whelan <whelandexter@gmail.com>
2025-07-30 17:50:29 +0100

    this is a starter on the automated testing, using vitest and simple scripts can target anything i want and see the test results automatically

---
+++ tests/api-client.test.ts
@@ -0,0 +1,33 @@
+import { describe, it, expect, vi } from 'vitest'
+import { makeRequest } from '../scripts/marsRobotAPIClientMode.js'
+import sampleInput from '../samples/sample-input.json'
+import sample_input_bad_terrain from '../samples/sample_input_bad_terrain.json'
+import sample_input_bad_command from '../samples/sample_input_bad_command.json'
+
+describe('makeRequest', () => {
+  it('should call API and return data', async () => {
+
+    const data = { sample: sampleInput }
+    const res = await makeRequest('localhost', 3000, '/api/simulate', data, false)
+    expect(res).toHaveProperty('statusCode')
+    expect(res).toHaveProperty('data')
+  })
+
+  it('should  return command failure for invalid terrain', async() => {
+    const badInput = sample_input_bad_terrain
+    
+    const res = await makeRequest('localhost', 3000, '/api/simulate', badInput, false)
+    expect(res.statusCode).toBe(400)
+    expect(res.data).toHaveProperty('error')
+    expect(res.data.error).toMatch(/terrain/i)
+  })
+
+  it('should  return command failure for invalid command', async() => {
+    const badInput = sample_input_bad_command
+    
+    const res = await makeRequest('localhost', 3000, '/api/simulate', badInput, false)
+    expect(res.statusCode).toBe(400)
+    expect(res.data).toHaveProperty('error')
+    expect(res.data.error).toMatch(/command/i)
+  })
+})
