c7b1b9aac8aab09639bd1594831d5e5a91a795ef
Dexter Whelan <whelandexter@gmail.com>
2025-07-30 12:51:01 +0100

    should've branched sooner. CLI is 'working' with node ./scripts/cli-processor.mjs ./sample-input.json output.json , lets format this into desired output now

--- scripts/cli-processor.mjs
+++ scripts/cli-processor.mjs
@@ -1,21 +1,22 @@
 #!/usr/bin/env node
 
 import fs from "fs/promises";
-import path from "path";
+import { fileURLToPath } from 'url';
+import { basename } from 'path';
+//some other imports here i'm not thinking of
 
-//some imports here i'm not thinking of
-
-import { simulateRobot } from "../lib/robot-simulator";
+import { MarsRobot, simulateRobot } from "../lib/robot-simulator.js";
 
 async function main() { 
     // break out args
+    console.log("1")
     const args = process.argv.slice(2)
     if (args.length !== 2){
         // may change this in future to be a typed version of params,right now it's only accepting input + output
         console.error("Usage: marsRobotCli.js <input.json> <output.json>");
         process.exit(1);
     };
-
+console.log("2")
     // process input data in arg
     const [inputPath, outputPath] = args
     
@@ -24,14 +25,18 @@ async function main() {
         // doule check encoding being enforced is better than no encoding
         const input = JSON.parse(inputJson);
 
+        console.log("3")
+
         // send to simulateRobot function
         const output = simulateRobot(input);
+        console.log("Simulation complete. Output:", output);
 
+        
         // await result?
 
         await fs.writeFile(outputPath, JSON.stringify(output, null, 2));
-
-        console.log("None.")
+        console.log("Done.")
+        
 
         // capture error?
     }catch (e){
@@ -41,6 +46,7 @@ async function main() {
     // exit/finish
 }
 
-if (process.argv[1] === new URL(import.meta.url).pathname){
+if (basename(fileURLToPath(import.meta.url)) === basename(process.argv[1])){
     main();
 }
+
